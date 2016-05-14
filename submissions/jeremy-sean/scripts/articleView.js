ac0b147ced150a7d9862e722f3fe3020d6f4534e
Jeremy Perez <perezje43@gmail.com>
2016-05-13 19:32:24 -0700

    added more css

--- submissions/jeremy-sean/scripts/articleView.js
+++ submissions/jeremy-sean/scripts/articleView.js
@@ -2,19 +2,25 @@
 var articleView = {};
 
 articleView.populateFilters = function() {
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var val = $(this).find('address a').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      $('#author-filter').append(optionTag);
-
-      val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
-      }
+  articles.forEach(function(article) {
+    $('#author-filter').append(article.toHtml('#author-filter-template'));
+    if($('#category-filter option:contains("' + article.category + '")').length === 0) {
+      $('#category-filter').append(article.toHtml('#category-filter-template'));
     }
   });
+  // $('article').each(function() {
+  //   if (!$(this).hasClass('template')) {
+  //     var val = $(this).find('address a').text();
+  //     var optionTag = '<option value="' + val + '">' + val + '</option>';
+  //     $('#author-filter').append(optionTag);
+  //
+  //     val = $(this).attr('data-category');
+  //     optionTag = '<option value="' + val + '">' + val + '</option>';
+  //     if ($('#category-filter option[value="' + val + '"]').length === 0) {
+  //       $('#category-filter').append(optionTag);
+  //     }
+  //   }
+  // });
 };
 
 articleView.handleAuthorFilter = function() {
@@ -61,7 +67,7 @@ articleView.setTeasers = function() {
     $(this).hide();
   });
 };
-
+console.log(articleView);
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
